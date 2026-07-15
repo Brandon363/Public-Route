a571bdb07e80623781ab08ee0c24f36a694e79bd
Brandon363 <brandonmutombwa@gmail.com>
2026-07-15 14:27:43 +0200

    added the missing uri

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    serviceFlowApiUrl: `https://serviceflow.api.dataalafrica.com`,
+    serviceFlowApiUrl: `https://serviceflow.api.dataalafrica.com/api/v1`,
     // serviceFlowApiUrl: `http://localhost:8015/api/v1`,
 };
